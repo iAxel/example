747339261a6e6494a6de611c8b48ab75d9af3881
axel <iaxel0297@gmail.com>
2021-09-23 14:13:34 +0500

    Add CreatesApplication Trait

---
+++ tests/Traits/CreatesApplication.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Traits;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Console\Kernel;
+
+trait CreatesApplication
+{
+    /**
+     * @return Application
+     */
+    public function createApplication(): Application
+    {
+        $app = require __DIR__ . '/../../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+}
