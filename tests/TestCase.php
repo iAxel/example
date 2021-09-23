5160b9e1dcd609246d6632cac1e168583f3e40ce
axel <iaxel0297@gmail.com>
2021-09-23 14:13:45 +0500

    Update TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use Tests\Traits\CreatesApplication;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    /**
+     * @var bool
+     */
+    protected bool $seed = true;
 }
