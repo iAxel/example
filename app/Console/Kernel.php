fe4bcb548573eb0891ba6cc14ea219e3e66f8645
axel <iaxel0297@gmail.com>
2021-09-24 17:25:42 +0500

    Cleanup Console Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,13 +3,12 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
+use Illuminate\Foundation\Console\Kernel as BaseKernel;
+
+final class Kernel extends BaseKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
      * @var array
      */
     protected $commands = [
@@ -17,25 +16,20 @@ class Kernel extends ConsoleKernel
     ];
 
     /**
-     * Define the application's command schedule.
+     * @param Schedule $schedule
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        //
     }
 
     /**
-     * Register the commands for the application.
-     *
      * @return void
      */
-    protected function commands()
+    protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
+        $this->load(__DIR__ . '/Commands');
     }
 }
