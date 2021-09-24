6b8fb0b1be09fc84cc03817ebb6cbf16ec43533b
axel <iaxel0297@gmail.com>
2021-09-25 00:05:51 +0500

    Update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace App\Providers;
 
+use InvalidArgumentException;
+
+use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -11,6 +18,32 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerValidators();
+    }
+
+    /**
+     * @return void
+     */
+    private function registerValidators(): void
+    {
+        Validator::extend('slug', function ($attributes, $value, $parameters) {
+            $slug = Str::slug($value);
+
+            $table = $parameters[0] ?? null;
+
+            $ignoreId = $parameters[1] ?? null;
+
+            if ($table === null) {
+                throw new InvalidArgumentException('First parameter does not exist.');
+            }
+
+            $query = DB::table($table)->where('slug', '=', $slug);
+
+            if ($ignoreId !== null) {
+                $query->where('id', '!=', $ignoreId);
+            }
+
+            return $query->exists() !== true;
+        });
     }
 }
