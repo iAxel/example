a498133fae9548d3a284c2e89605c51c987f34ef
axel <iaxel0297@gmail.com>
2021-09-24 22:01:36 +0500

    Update User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,59 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use App\Models\Scopes\HasId;
+use App\Models\Scopes\HasEmail;
 
-class User extends Authenticatable
+use App\Models\Traits\HasCUD;
+use App\Models\Traits\HasDate;
+
+use Illuminate\Support\Carbon;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use App\Models\Auth\Authenticatable;
+use App\Models\Auth\Gate\Authorizable;
+
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ *
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ *
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @mixin Model
+ */
+final class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasId;
+    use HasEmail;
+
+    use HasCUD;
+    use HasDate;
+
+    use HasFactory;
+
+    use Authorizable;
+    use Authenticatable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * @var array
      */
     protected $fillable = [
         'name',
@@ -24,21 +63,45 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
      * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'id' => 'int',
+        'name' => 'string',
+        'email' => 'string',
+        'password' => 'string',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
     ];
+
+    /**
+     * @param array $attributes
+     *
+     * @return Builder
+     */
+    public function filter(array $attributes): Builder
+    {
+        return $this->newQuery()
+            ->when($attributes['search'] ?? null, function ($query, $search) {
+                return $query
+                    ->where('id', 'like', "%$search%")
+                    ->orWhere('name', 'like', "%$search%")
+                    ->orWhere('email', 'like', "%$search%");
+            })
+            ->when($attributes['name'] ?? null, function ($query, $name) {
+                return $query->where('name', 'like', "%$name%");
+            })
+            ->when($attributes['email'] ?? null, function ($query, $email) {
+                return $query->where('email', 'like', "%$email%");
+            })
+            ->when($attributes['date'] ?? null, function ($query, $date) {
+                return $query->whereBetween('created_at', [
+                    $this->getDate($date['from']),
+                    $this->getDate($date['to']),
+                ]);
+            })
+            ->when($attributes['sort'] ?? null, function ($query, $sort) {
+                return $query->orderBy($sort['by'], $sort['type']);
+            });
+    }
 }
