bd7625368c04b980c35a72256c254ab86b9f21f2
axel <iaxel0297@gmail.com>
2021-09-24 21:59:44 +0500

    Remove Authenticate Middleware Remove EncryptCookies Middleware Remove VerifyCsrfToken Middleware Remove RedirectIfAuthenticated Middleware Remove PreventRequestsDuringMaintenance Middleware Update TrustHosts Middleware Update TrustProxies Middleware Update TrimStrings Middleware

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -4,16 +4,14 @@ namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\TrimStrings as Middleware;
 
-class TrimStrings extends Middleware
+final class TrimStrings extends Middleware
 {
     /**
-     * The names of the attributes that should not be trimmed.
-     *
      * @var array
      */
     protected $except = [
-        'current_password',
         'password',
         'password_confirmation',
+        'current_password',
     ];
 }
--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,27 +2,19 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request;
 
-class TrustProxies extends Middleware
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
+final class TrustProxies extends Middleware
 {
     /**
-     * The trusted proxies for this application.
-     *
      * @var array|string|null
      */
     protected $proxies;
 
     /**
-     * The headers that should be used to detect proxies.
-     *
      * @var int
      */
-    protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::HEADER_X_FORWARDED_AWS_ELB;
+    protected $headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB;
 }
