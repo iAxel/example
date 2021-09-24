632c105bc38ce472111b89f2fe47895ca23f1722
axel <iaxel0297@gmail.com>
2021-09-24 17:16:15 +0500

    Cleanup configs

--- config/cache.php
+++ config/cache.php
@@ -1,18 +1,11 @@
 <?php
 
-use Illuminate\Support\Str;
-
 return [
 
     /*
     |--------------------------------------------------------------------------
     | Default Cache Store
     |--------------------------------------------------------------------------
-    |
-    | This option controls the default cache connection that gets used while
-    | using this caching library. This connection is used when another is
-    | not explicitly specified when executing a given caching function.
-    |
     */
 
     'default' => env('CACHE_DRIVER', 'file'),
@@ -21,18 +14,9 @@ return [
     |--------------------------------------------------------------------------
     | Cache Stores
     |--------------------------------------------------------------------------
-    |
-    | Here you may define all of the cache "stores" for your application as
-    | well as their drivers. You may even define multiple stores for the
-    | same cache driver to group types of items stored in your caches.
-    |
-    | Supported drivers: "apc", "array", "database", "file",
-    |         "memcached", "redis", "dynamodb", "octane", "null"
-    |
     */
 
     'stores' => [
-
         'apc' => [
             'driver' => 'apc',
         ],
@@ -56,14 +40,18 @@ return [
 
         'memcached' => [
             'driver' => 'memcached',
+
             'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
+
             'sasl' => [
                 env('MEMCACHED_USERNAME'),
                 env('MEMCACHED_PASSWORD'),
             ],
+
             'options' => [
-                // Memcached::OPT_CONNECT_TIMEOUT => 2000,
+                //
             ],
+
             'servers' => [
                 [
                     'host' => env('MEMCACHED_HOST', '127.0.0.1'),
@@ -91,20 +79,13 @@ return [
         'octane' => [
             'driver' => 'octane',
         ],
-
     ],
 
     /*
     |--------------------------------------------------------------------------
     | Cache Key Prefix
     |--------------------------------------------------------------------------
-    |
-    | When utilizing a RAM based store such as APC or Memcached, there might
-    | be other applications utilizing the same cache. So, we'll specify a
-    | value to get prefixed to all our keys so we can avoid collisions.
-    |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
-
+    'prefix' => env('CACHE_PREFIX', 'example_cache'),
 ];
--- config/logging.php
+++ config/logging.php
@@ -1,20 +1,11 @@
 <?php
 
-use Monolog\Handler\NullHandler;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\SyslogUdpHandler;
-
 return [
 
     /*
     |--------------------------------------------------------------------------
     | Default Log Channel
     |--------------------------------------------------------------------------
-    |
-    | This option defines the default log channel that gets used when writing
-    | messages to the logs. The name specified in this option should match
-    | one of the channels defined in the "channels" configuration array.
-    |
     */
 
     'default' => env('LOG_CHANNEL', 'stack'),
@@ -23,21 +14,16 @@ return [
     |--------------------------------------------------------------------------
     | Log Channels
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure the log channels for your application. Out of
-    | the box, Laravel uses the Monolog PHP logging library. This gives
-    | you a variety of powerful log handlers / formatters to utilize.
-    |
-    | Available Drivers: "single", "daily", "slack", "syslog",
-    |                    "errorlog", "monolog",
-    |                    "custom", "stack"
-    |
     */
 
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+
+            'channels' => [
+                'single',
+            ],
+
             'ignore_exceptions' => false,
         ],
 
@@ -65,7 +51,8 @@ return [
         'papertrail' => [
             'driver' => 'monolog',
             'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => SyslogUdpHandler::class,
+            'handler' => Monolog\Handler\SyslogUdpHandler::class,
+
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
@@ -75,8 +62,9 @@ return [
         'stderr' => [
             'driver' => 'monolog',
             'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => StreamHandler::class,
+            'handler' => Monolog\Handler\StreamHandler::class,
             'formatter' => env('LOG_STDERR_FORMATTER'),
+
             'with' => [
                 'stream' => 'php://stderr',
             ],
@@ -94,12 +82,11 @@ return [
 
         'null' => [
             'driver' => 'monolog',
-            'handler' => NullHandler::class,
+            'handler' => Monolog\Handler\NullHandler::class,
         ],
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
     ],
-
 ];
