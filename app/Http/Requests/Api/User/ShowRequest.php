97f6f49fd1f73105a856ed5be221432f2feba321
axel <iaxel0297@gmail.com>
2021-09-25 20:45:59 +0500

    Update User Requests

--- app/Http/Requests/Api/User/ShowRequest.php
+++ app/Http/Requests/Api/User/ShowRequest.php
@@ -4,14 +4,18 @@ namespace App\Http\Requests\Api\User;
 
 use App\Http\Requests\Api\Request;
 
+use App\Http\Requests\Traits\HasId;
+
 final class ShowRequest extends Request
 {
+    use HasId;
+
     /**
      * @return bool
      */
     public function authorize(): bool
     {
-        return true;
+        return $this->user()->can('view', $this->getId());
     }
 
     /**
