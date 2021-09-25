b829be6a34f7ec394793da308415c1b69482d37c
axel <iaxel0297@gmail.com>
2021-09-25 19:17:50 +0500

    Add IndexRequest For Profile Session

---
+++ app/Http/Requests/Api/Profile/Session/IndexRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Api\Profile\Session;
+
+use App\Http\Requests\Api\Request;
+
+use App\Http\Requests\Traits\HasToken;
+
+final class IndexRequest extends Request
+{
+    use HasToken;
+
+    /**
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [];
+    }
+}
