05dd973a95c064a6f9d9357b7329f0662951d1c9
axel <iaxel0297@gmail.com>
2021-09-25 20:45:06 +0500

    Update HasRoles Relation For User Model

--- app/Models/Relations/User/HasRoles.php
+++ app/Models/Relations/User/HasRoles.php
@@ -15,4 +15,33 @@ trait HasRoles
     {
         return $this->belongsToMany(Role::class, 'user_roles', 'user_id', 'role_id', 'id', 'id', 'roles');
     }
+
+    /**
+     * @param string $role
+     *
+     * @return bool
+     */
+    public function hasRole(string $role): bool
+    {
+        return $this->roles()->where('slug', '=', $role)->exists();
+    }
+
+    /**
+     * @param string $policy
+     * @param string $permission
+     *
+     * @return bool
+     */
+    public function hasPermission(string $policy, string $permission): bool
+    {
+        $this->loadMissing([
+            'roles.permissions',
+            'roles.permissions.policy',
+        ]);
+
+        return $this->roles->containsStrict([
+            'permissions.*.slug', $permission,
+            'permissions.*.policy.slug', $policy,
+        ]);
+    }
 }
