1e121c39819d0b1864ddc8aaf4ecc6aa48e40a7d
axel <iaxel0297@gmail.com>
2021-09-25 17:49:47 +0500

    Update AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
+
+use App\Auth\TokenGuard;
+use App\Auth\TokenProvider;
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 final class AuthServiceProvider extends ServiceProvider
@@ -18,6 +23,24 @@ final class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->registerGuards();
+
         $this->registerPolicies();
+    }
+
+    /**
+     * @return void
+     */
+    private function registerGuards(): void
+    {
+        Auth::extend('olympus', function ($app, $name, $config) {
+            $provider = $app->make(TokenProvider::class);
+
+            $guard = new TokenGuard($provider, $app['request'], $config);
+
+            $app->refresh('request', $guard, 'setRequest');
+
+            return $guard;
+        });
     }
 }
