5156e948f0ca8c2ec70a125e2e831d4d60dab3fd
axel <iaxel0297@gmail.com>
2021-09-25 16:06:49 +0500

    Add BelongToMany Migration Tables

---
+++ database/migrations/2021_01_01_000005_create_user_roles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserRolesTable extends Migration
+{
+    /**
+     * @var string
+     */
+    protected string $table = 'user_roles';
+
+    /**
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+        });
+
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->unique([
+                'user_id',
+                'role_id',
+            ]);
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists($this->table);
+    }
+}
